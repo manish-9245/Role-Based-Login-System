371a17cbce279e1ce53bc24609c83852e06a6c9e
Manish Tiwari <69393822+manish-9245@users.noreply.github.com>
2021-07-24 17:35:42 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username="root";
 $password="";
-$dbname="jpmc";
+$dbname="type any name of your choice";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
